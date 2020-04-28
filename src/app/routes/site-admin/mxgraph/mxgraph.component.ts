6a8bb2fb89a48a4c0b38cdad979501e789b0506c
Wangwei <550443096@qq.com>
2020-04-28 21:04:32 +0800

    身份认证修复bug

--- src/app/routes/site-admin/mxgraph/mxgraph.component.ts
+++ src/app/routes/site-admin/mxgraph/mxgraph.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
 import { MxgraphAuthService } from '@shared/mxgraph/mxgraph-auth.service';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -10,7 +11,8 @@ import { MxgraphAuthService } from '@shared/mxgraph/mxgraph-auth.service';
 export class SiteAdminMxgraphComponent implements OnInit {
 
   constructor(private http: _HttpClient,
-              private mxGraphAuthService: MxgraphAuthService) {
+              private mxGraphAuthService: MxgraphAuthService,
+              public router:ActivatedRoute) {
   }
 
 
@@ -18,25 +20,28 @@ export class SiteAdminMxgraphComponent implements OnInit {
     // 每次渲染时，这段函数都会执行
     let mxGraphRoot = 'http://localhost:8089/xian';
     //let mxGraphRoot = 'http://localhost:8080/javascript/examples/grapheditor/www/index.html';
-
-    // 检查由表格跳转的url是否携带参数,若不携带则定位到起始页
-    if (window.location.href.indexOf('?') == -1) {
-      document.getElementById('mxgraph')["src"] = mxGraphRoot;
-      return;
-    }else{
-      // 若携带参数，则获取站点参数
-      let params = window.location.href.substring(window.location.href.lastIndexOf('?'));
-      /**
-       * 在这里生成链接mxGraph的Url地址，需经过UrlDecode转码，防止中文乱码，请无视CLI控制台的报错
-       * 请在mxGraph项目的action_ww.js中完善$(function(){})内的代码，以实现根据url显式参数打开某个组态图
-       * url-demo: http://localhost:8080/javascript/examples/grapheditor/www/index.html?site_name=郑州&site_level=1
-       */
-      let targetMxGraphUrl = mxGraphRoot + decodeURIComponent(params);
-      //alert(targetMxGraphUrl);
-      let iframe = document.getElementById('mxgraph');
-      iframe["src"] = targetMxGraphUrl;
-      this.mxGraphAuthService.doHandle(iframe);
-    }
+    // 获取iframe
+    let iframe = document.getElementById('mxgraph');
+    // 检测query参数
+    this.router.queryParams.subscribe((data) => {
+      if(data.sitename && data.sitelevel)
+      {
+        // 若携带参数，则获取站点参数
+        let params = window.location.href.substring(window.location.href.lastIndexOf('?'));
+        /**
+         * 在这里生成链接mxGraph的Url地址，需经过UrlDecode转码，防止中文乱码，请无视CLI控制台的报错
+         * 请在mxGraph项目的action_ww.js中完善$(function(){})内的代码，以实现根据url显式参数打开某个组态图
+         * url-demo: http://localhost:8080/javascript/examples/grapheditor/www/index.html?site_name=郑州&site_level=1
+         */
+        let targetMxGraphUrl = mxGraphRoot + decodeURIComponent(params);
+        //alert(targetMxGraphUrl);
+        iframe["src"] = targetMxGraphUrl;
+      }
+      else {
+        iframe["src"] = mxGraphRoot;
+      }
+    });
+    this.mxGraphAuthService.doHandle(iframe);
   }
 
 }
