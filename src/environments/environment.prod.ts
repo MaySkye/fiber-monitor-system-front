d92a30afa35d9901259674eba1c1d93781fb1721
Wangwei <550443096@qq.com>
2020-05-21 23:30:11 +0800

    environment.prod.ts修改http根

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // SERVER_URL: `http://192.168.99.15:8080/fiberserver/`,
-  SERVER_URL: `http://192.168.0.1:8888/`,
+  SERVER_URL: `http://127.0.0.1:8888/`,
   production: true,
   useHash: true,
   hmr: false,
