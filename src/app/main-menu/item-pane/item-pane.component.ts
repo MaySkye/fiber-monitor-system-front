a4b02e1c7a9e616a6687f7db8fdc3ec5d2630e51
Wangwei <550443096@qq.com>
2020-06-09 22:14:57 +0800

    整合主菜单

--- src/app/main-menu/item-pane/item-pane.component.ts
+++ src/app/main-menu/item-pane/item-pane.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ContentChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'mm-item-pane',
   templateUrl: './item-pane.component.html',
   styleUrls: ['item-pane.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ItemPaneComponent implements OnInit {
 
-  @Input('title') title: string;
-  @Input('detail') detail: string;
+  @ContentChild('content') content: TemplateRef<any>;
+
 
   constructor() {
   }
---
+++ src/app/main-menu/main-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.less'],
+})
+export class MainMenuComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+  }
+
+}
